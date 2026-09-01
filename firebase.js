24c84b17aeb06e5f450c6a5a4fe2ed334860c43b
kulwantsingh637555-cpu <kulwantsingh637555@gmail.com>
2026-09-01 15:13:18 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -3,15 +3,16 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDM_2MYad1LPpEBPEQhkhZKsdy20WpQoww",
+  apiKey: "AIzaSyDM_2MYad1LPpEBPEQhkhZKsdY20WpQoww",
   authDomain: "smart-hotel-veggis-store.firebaseapp.com",
   projectId: "smart-hotel-veggis-store",
   storageBucket: "smart-hotel-veggis-store.firebasestorage.app",
   messagingSenderId: "313226803394",
-  appId: "1:313226803394:web:64915703ffb27a673c635b",
-  measurementId: "G-CMXXCF84QN"
+  appId: "1:313226803394:web:ab9469c5c65098993c635b",
+  measurementId: "G-K3WGRGMG17"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
